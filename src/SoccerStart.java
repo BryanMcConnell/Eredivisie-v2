e0d2654fbb1466e17c2fc9d7dda87cf4564adcbd
BryanMcConnell <mcconnell.bryan@protonmail.com>
2021-12-20 17:09:40 -0600

    update2

--- src/SoccerStart.java
+++ src/SoccerStart.java
@@ -9,19 +9,18 @@ public class SoccerStart {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);		
-		Team ajax = new Team("Ajax", "Amsterdam", 15, 12, 1);
-		Team psv = new Team("PSV", "Eindhoven", 13, 13, 2);
-		Team feyenoord = new Team("Feyenord", "Rotterdam", 14, 10, 3);
 		
 		System.out.println("Menu (select number): \n1: Team List\n2: City List\n3: League Table\n4: Simulate a Match");
 		int userMenu = input.nextInt();
 		
 		
 		if (userMenu == 1) {
-			SoccerTeam menu = new SoccerTeam();
-			menu.Team();
+			SoccerTeam listTeam = new SoccerTeam();
+			listTeam.teamList();
+			
 		}
-		
+		 
 	}
 	
 }
+
--- src/SoccerTeam.java
+++ src/SoccerTeam.java
@@ -30,6 +30,15 @@ public class SoccerTeam {
 		int newTableRank = input.nextInt();
 		return newTableRank;
 	}
+	
+	protected void teamList() {
+		
+		Team ajax = new Team("Ajax", "Amsterdam", 15, 12, 1);
+		Team psv = new Team("PSV", "Eindhoven", 13, 13, 2);
+		Team feyenoord = new Team("Feyenord", "Rotterdam", 14, 10, 3);
+		System.out.println(ajax.teamName + "\n" + psv.teamName + "\n" + feyenoord.teamName);
+	}
+	
 }
 
 class Team extends SoccerTeam {
@@ -40,7 +49,19 @@ class Team extends SoccerTeam {
 		this.defRating = defRating;
 		this.tableRank = tableRank;
 		
+		
+		
 	}
 	
-
 }
+
+/** class MenuOne {
+	
+	public void oneMenu() {
+		
+		SoccerTeam listT = new SoccerTeam();
+		System.out.println(listT.teamList());
+		
+	}
+	
+} **/
